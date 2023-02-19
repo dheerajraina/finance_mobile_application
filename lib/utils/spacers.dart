a4c2b8a00ebb968c39308a16d56902c5a69b5250
Dheeraj <dheerajraina55@gmail.com>
2023-02-19 15:07:20 +0530

    Commit 3
    Spacers

---
+++ lib/utils/spacers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget verticalSpacer(height) {
+  return SizedBox(
+    height: height,
+  );
+}
+
+Widget horizontalSpacer(width) {
+  return SizedBox(
+    width: width,
+  );
+}
